78f4eee940d8521d516c7f35667c451dcddfd485
jaein <jaein@jellybus.com>
2024-01-25 19:18:56 +0900

    [FilterJIN-4] Gallery 연동 + 이미지 그레이스케일 픽셀별 연산 구현

--- app/src/main/java/com/example/filterjin/FilterItem.kt
+++ app/src/main/java/com/example/filterjin/FilterItem.kt
@@ -3,4 +3,4 @@ package com.example.filterjin
 import android.media.Image
 import android.widget.ImageView
 
-data class FilterItem ( val id : Int, val name : String, val imagePath : String )
+data class FilterItem ( val id : Int, val name : String, val thumbnail : Int , val imagePath : String )
--- app/src/main/java/com/example/filterjin/MainActivity.kt
+++ app/src/main/java/com/example/filterjin/MainActivity.kt
@@ -1,26 +1,42 @@
 package com.example.filterjin
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainLayout: MainLayout
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mainLayout = MainLayout(this).getMainLayout()
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // 선택된 이미지의 URI를 가져옴
+                val imageUri = result.data?.data
+                // MainLayout의 setImage 메소드를 호출하여 imageView에 이미지를 설정
+                mainLayout.loadImage(imageUri)
+            }
+        }
 
-        setContentView(mainLayout)
-
+        mainLayout = MainLayout(this, galleryLauncher)
+        setContentView(mainLayout.getMainLayout())
     }
 
 
 
-}
+    }
+
+
