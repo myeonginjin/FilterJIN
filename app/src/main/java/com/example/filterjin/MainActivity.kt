79a838adae9458bb715b8e6f941b4c14447df6a9
jaein <jaein@jellybus.com>
2024-02-08 19:02:22 +0900

    [FilterJIN-13] 이미지 리사이징 통한 프리뷰 이미지 구현

--- app/src/main/java/com/example/filterjin/MainActivity.kt
+++ app/src/main/java/com/example/filterjin/MainActivity.kt
@@ -10,14 +10,8 @@ import android.media.ExifInterface
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,21 +25,49 @@ class MainActivity : AppCompatActivity() {
         //카메라 앱 엑티비티에서 돌아올 때 안드로이드 시스템의 의해 ActivityResultCallback의 onActivity()함수 자동 실행
         galleryLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                //카메라 엑티비티 결과 정상인지 확인
-                if (result.resultCode == Activity.RESULT_OK) {
-                    // 선택된 이미지의 URI를 가져옴
-                    val imageUri = result.data?.data
-                    if (imageUri != null) {
-                        // 이미지의 회전을 처리
-                        val inputStream = contentResolver.openInputStream(imageUri)
-                        val bitmap = BitmapFactory.decodeStream(inputStream)
-                        val rotatedBitmap = rotateImageIfRequired(this, bitmap, imageUri)
+                try {
+                    if (result.resultCode == Activity.RESULT_OK) {
+                        val imageUri = result.data?.data
+                        if (imageUri != null) {
+                            // 원본 이미지 로드 및 회전 처리
+                            var rotatedOriginalBitmap: Bitmap? = null
+                            contentResolver.openInputStream(imageUri)?.use { inputStream ->
+                                val originalBitmap = BitmapFactory.decodeStream(inputStream)
+                                rotatedOriginalBitmap = rotateImageIfRequired(this, originalBitmap, imageUri)
+                            }
 
-                        // MainLayout의 loadImage 메소드를 호출하여 imageView에 이미지를 설정
-                        mainLayout.loadImage(rotatedBitmap)
+                            // 리사이징 작업 및 리사이징된 이미지의 회전 처리
+                            var rotatedResizedBitmap: Bitmap? = null
+                            contentResolver.openInputStream(imageUri)?.use { inputStream ->
+                                // 적절한 inSampleSize 계산
+                                val options = BitmapFactory.Options().apply { inJustDecodeBounds = true }
+                                BitmapFactory.decodeStream(inputStream, null, options)
+                                val calRatio = calculateSampleSize(
+                                    options,
+                                    resources.getDimensionPixelSize(R.dimen.imgSize),
+                                    resources.getDimensionPixelSize(R.dimen.imgSize)
+                                )
+                                // 실제 리사이징을 위해 inSampleSize 설정
+                                options.apply {
+                                    inJustDecodeBounds = false
+                                    inSampleSize = calRatio
+                                }
+                                // 리사이징된 이미지 생성
+                                val resizedBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri), null, options)
+                                rotatedResizedBitmap = resizedBitmap?.let { rotateImageIfRequired(this, it, imageUri) }
+                            }
+
+                            // MainLayout에 원본 및 리사이징된 이미지 설정
+                            if (rotatedOriginalBitmap != null && rotatedResizedBitmap != null) {
+                                mainLayout.setImage(rotatedOriginalBitmap!!, rotatedResizedBitmap!!)
+                            }
+                        }
                     }
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
             }
+
 
         //registerForActivityResult는 ComponentActivity의 메서드이기 때문에 Activity가 아닌 MainLayout 클래스 내에선 직접 호출 불가능. 인자로 전달해줌
         mainLayout = MainLayout(this, galleryLauncher)
@@ -72,7 +94,28 @@ class MainActivity : AppCompatActivity() {
         return Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true).also {
             if (it != img) img.recycle()
             }
-        }
     }
+
+
+    private fun calculateSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
+    }
+
+
+
+}
+
+
 
 
